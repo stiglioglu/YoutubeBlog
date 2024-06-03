f0365ffd2c7e20ef1ab51b4f7e0e27f4a13c385b
Şükrücan TIĞLIOĞLU <54224494+stiglioglu@users.noreply.github.com>
2024-06-03 05:58:28 +0300

    Add Find logged in users - ClaimsPrincipal

--- YoutubeBlog.Service/Extensions/ServiceLayerExtensions.cs
+++ YoutubeBlog.Service/Extensions/ServiceLayerExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System.Globalization;
 using System.Reflection;
@@ -17,6 +18,9 @@ namespace YoutubeBlog.Service.Extensions
             
             services.AddScoped<IArticleService,ArticleService>();
             services.AddScoped<ICategoryService,CategoryService>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
             services.AddAutoMapper(assembly);
 
             services.AddControllersWithViews().AddFluentValidation(opt =>
--- YoutubeBlog.Service/Services/Concrete/ArticleService.cs
+++ YoutubeBlog.Service/Services/Concrete/ArticleService.cs
@@ -1,12 +1,10 @@
 ﻿using AutoMapper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
 using YoutubeBlog.Data.UnitOfWorks;
 using YoutubeBlog.Entity.DTOs.Articles;
 using YoutubeBlog.Entity.Entities;
+using YoutubeBlog.Service.Extensions;
 using YoutubeBlog.Service.Services.Abstractions;
 
 namespace YoutubeBlog.Service.Services.Concrete
@@ -15,27 +13,32 @@ namespace YoutubeBlog.Service.Services.Concrete
     {
         private readonly IUnitOfWork unitOfWork;
         private readonly IMapper mapper;
+        private readonly IHttpContextAccessor httpContextAccessor;
+        private readonly ClaimsPrincipal _user;
 
-        public ArticleService(IUnitOfWork unitOfWork, IMapper mapper)
+        public ArticleService(IUnitOfWork unitOfWork, IMapper mapper, IHttpContextAccessor httpContextAccessor)
         {
             this.unitOfWork = unitOfWork;
             this.mapper = mapper;
+            this.httpContextAccessor = httpContextAccessor;
+            _user = httpContextAccessor.HttpContext.User;
         }
-
         public async Task CreateArticleAsync(ArticleAddDto articleAddDto)
         {
-            var userId = Guid.Parse("93005E5F-433A-4D09-9F3E-5702DF10F77B");
+            var userId = _user.GetLoggedInUserId();
+            var userEmail = _user.GetLoggedInEmail();
             var imageId = Guid.Parse("FC736561-06D2-473E-B4C7-FEA7884A488E");
             var article = new Article(
-                articleAddDto.Title, 
-                articleAddDto.Content, 
-                userId,articleAddDto.CategoryId, 
+                articleAddDto.Title,
+                articleAddDto.Content,
+                userId,
+                userEmail,
+                articleAddDto.CategoryId,
                 imageId
             );
             await unitOfWork.GetRepository<Article>().AddAsync(article);
             await unitOfWork.SaveAsync();
         }
-
         public async Task<List<ArticleDto>> GetAllArticlesWithCategoryNonDeletedAsync()
         {
             var articles = await unitOfWork.GetRepository<Article>().GetAllAsync(x=>x.IsDeleted==false,x=>x.Category);
@@ -52,11 +55,15 @@ namespace YoutubeBlog.Service.Services.Concrete
         }
         public async Task<string> UpdateArticleAsync(ArticleUpdateDto articleUpdateDto)
         {
+            var userEmail = _user.GetLoggedInEmail();
+
             var article = await unitOfWork.GetRepository<Article>().GetAsync(x => x.IsDeleted == false && x.Id == articleUpdateDto.Id, x => x.Category);
 
             article.Title = articleUpdateDto.Title;
             article.Content = articleUpdateDto.Content;
             article.CategoryId = articleUpdateDto.CategoryId;
+            article.ModifiedDate = DateTime.Now;
+            article.ModifiedBy = userEmail;
 
             await unitOfWork.GetRepository<Article>().UpdateAsync(article);
             await unitOfWork.SaveAsync();
@@ -65,9 +72,13 @@ namespace YoutubeBlog.Service.Services.Concrete
         }
         public async Task<string> SafeDeleteArticleAsync(Guid articleId)
         {
+            var userEmail = _user.GetLoggedInEmail();
+
             var article = await unitOfWork.GetRepository<Article>().GetByGuidAsync(articleId);
+
             article.IsDeleted = true;
             article.DeletedDate = DateTime.Now;
+            article.DeletedBy = userEmail;
 
             await unitOfWork.GetRepository<Article>().UpdateAsync(article);
             await unitOfWork.SaveAsync();
