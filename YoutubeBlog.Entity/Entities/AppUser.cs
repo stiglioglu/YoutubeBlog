d9b18dc6ce945ae4ce08255481baac04ad69a08d
Şükrücan TIĞLIOĞLU <54224494+stiglioglu@users.noreply.github.com>
2024-06-21 14:09:20 +0300

    Added user add

--- YoutubeBlog.Entity/Entities/AppUser.cs
+++ YoutubeBlog.Entity/Entities/AppUser.cs
@@ -11,7 +11,7 @@ namespace YoutubeBlog.Entity.Entities
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public Guid ImageId { get; set; }
+        public Guid ImageId { get; set; } = Guid.Parse("76756DAA-E0EF-4ED8-97CD-A9CBC0A9A50A");
         public Image Image { get; set; }
         public ICollection<Article> Articles { get; set; }
         public ICollection<AppUser> Users { get; set; }
--- YoutubeBlog.Web/Areas/Admin/Controllers/UserController.cs
+++ YoutubeBlog.Web/Areas/Admin/Controllers/UserController.cs
@@ -2,8 +2,11 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NToastNotify;
+using YoutubeBlog.Entity.DTOs.Articles;
 using YoutubeBlog.Entity.DTOs.Users;
 using YoutubeBlog.Entity.Entities;
+using YoutubeBlog.Web.ResultMessages;
 
 namespace YoutubeBlog.Web.Areas.Admin.Controllers
 {
@@ -11,11 +14,15 @@ namespace YoutubeBlog.Web.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly UserManager<AppUser> userManager;
+        private readonly RoleManager<AppRole> roleManager;
+        private readonly IToastNotification toast;
         private readonly IMapper mapper;
 
-        public UserController(UserManager<AppUser> userManager, IMapper mapper)
+        public UserController(UserManager<AppUser> userManager,RoleManager<AppRole> roleManager,IToastNotification toast, IMapper mapper)
         {
             this.userManager = userManager;
+            this.roleManager = roleManager;
+            this.toast = toast;
             this.mapper = mapper;
         }
 
@@ -33,6 +40,43 @@ namespace YoutubeBlog.Web.Areas.Admin.Controllers
             }
 
             return View(map);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var roles = await roleManager.Roles.ToListAsync();
+            return View(new UserAddDto { Roles = roles });
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(UserAddDto userAddDto)
+        {
+            var map = mapper.Map<AppUser>(userAddDto);
+            var roles = await roleManager.Roles.ToListAsync();
+
+            if (ModelState.IsValid)
+            {
+                map.UserName = userAddDto.Email;
+                var result = await userManager.CreateAsync(map, string.IsNullOrEmpty(userAddDto.Password)?"": userAddDto.Password);
+                if (result.Succeeded)
+                {
+                    var findRole = await roleManager.FindByIdAsync(userAddDto.RoleId.ToString());
+                    await userManager.AddToRoleAsync(map, findRole.ToString());
+                    toast.AddSuccessToastMessage(
+                    Messages.User.Add(userAddDto.Email),
+                    new ToastrOptions { Title = "Başarılı!" }
+                );
+                    return RedirectToAction("Index", "Article", new { Area = "Admin" });
+                }
+                else
+                {
+                    foreach (var errors in result.Errors)
+                    {
+                        ModelState.AddModelError("", errors.Description);
+                    }
+                    return View(new UserAddDto { Roles = roles });
+                }
+            }
+            return View(new UserAddDto { Roles = roles });
         }
     }
 }
